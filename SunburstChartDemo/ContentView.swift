4a481e5c18512aecefb2da18972535d3ad913a53
Danny Bolella <dbolella@gmail.com>
2024-05-01 23:42:35 -0400

    Initial Commit

---
+++ SunburstChartDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SunburstChartDemo
+//
+//  Created by Daniel Bolella on 5/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SunburstChartDemo/SunburstChartDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunburstChartDemoApp.swift
+//  SunburstChartDemo
+//
+//  Created by Daniel Bolella on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SunburstChartDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
